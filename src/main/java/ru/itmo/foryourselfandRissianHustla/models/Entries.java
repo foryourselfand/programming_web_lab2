c051d9947093b891cc7836792079ced3a7d018eb
foryourselfand <foryourselfand@gmail.com>
2020-10-13 11:11:38 +0300

    equals, hashCode and toString in models

--- src/main/java/ru/itmo/foryourselfandRissianHustla/models/Entries.java
+++ src/main/java/ru/itmo/foryourselfandRissianHustla/models/Entries.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Objects;
 
 public class Entries implements Serializable {
 	private final Deque<Entry> entries;
@@ -24,5 +25,26 @@ public class Entries implements Serializable {
 	
 	public SimpleDateFormat getSimpleDateFormat() {
 		return simpleDateFormat;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (! (o instanceof Entries)) return false;
+		Entries entries1 = (Entries) o;
+		return Objects.equals(getEntries(), entries1.getEntries()) && Objects.equals(getSimpleDateFormat(), entries1.getSimpleDateFormat());
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(getEntries(), getSimpleDateFormat());
+	}
+	
+	@Override
+	public String toString() {
+		return "Entries{" +
+				"entries=" + entries +
+				", simpleDateFormat=" + simpleDateFormat +
+				'}';
 	}
 }
--- src/main/java/ru/itmo/foryourselfandRissianHustla/models/Entry.java
+++ src/main/java/ru/itmo/foryourselfandRissianHustla/models/Entry.java
@@ -2,6 +2,7 @@ package ru.itmo.foryourselfandRissianHustla.models;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 public class Entry implements Serializable {
     private double x;
@@ -56,5 +57,29 @@ public class Entry implements Serializable {
     
     public void setQueryTime(Date queryTime) {
         this.queryTime = queryTime;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (! (o instanceof Entry)) return false;
+        Entry entry = (Entry) o;
+        return Double.compare(entry.getX(), getX()) == 0 && Double.compare(entry.getY(), getY()) == 0 && Double.compare(entry.getR(), getR()) == 0 && isResult() == entry.isResult() && Objects.equals(getQueryTime(), entry.getQueryTime());
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY(), getR(), isResult(), getQueryTime());
+    }
+    
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "x=" + x +
+                ", y=" + y +
+                ", r=" + r +
+                ", result=" + result +
+                ", queryTime=" + queryTime +
+                '}';
     }
 }
